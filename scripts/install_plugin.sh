fe00e7862e5268f023c5f8ac0faedf78bd83b5c0
bellkeyang <bellkeyang@tencent.com>
2022-01-18 14:58:55 +0800

    feat: update plugin content

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -8,15 +8,15 @@ if [ -n "${HELM_LINTER_PLUGIN_NO_INSTALL_HOOK}" ]; then
 fi
 
 version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
-echo "Downloading and installing teststack ${version} ..."
+echo "Downloading and installing bcstest ${version} ..."
 
 url=""
 if [ "$(uname)" = "Darwin" ]; then
-    url="https://github.com/dellkeji/helm-test/releases/download/v${version}/teststack_${version}_darwin_amd64.tar.gz"
+    url="https://github.com/dellkeji/helm-bcstest/releases/download/v${version}/bcstest_${version}_darwin_amd64.tar.gz"
 elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com/dellkeji/helm-test/releases/download/v${version}/teststack_${version}_linux_amd64.tar.gz"
+    url="https://github.com/dellkeji/helm-bcstest/releases/download/v${version}/bcstest_${version}_linux_amd64.tar.gz"
 else
-    url="https://github.com/dellkeji/helm-test/releases/download/v${version}/teststack_${version}_windows_amd64.tar.gz"
+    url="https://github.com/dellkeji/helm-bcstest/releases/download/v${version}/bcstest_${version}_windows_amd64.tar.gz"
 fi
 
 echo "$url"
@@ -31,5 +31,5 @@ else
     wget -q "${url}" -O "releases/${version}.tar.gz"
 fi
 tar xzf "releases/${version}.tar.gz" -C "releases/${version}"
-mv "releases/${version}/teststack" "bin/teststack" || \
-    mv "releases/${version}/teststack.exe" "bin/teststack"
+mv "releases/${version}/bcstest" "bin/bcstest" || \
+    mv "releases/${version}/bcstest.exe" "bin/bcstest"
